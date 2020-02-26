ca7fbcb38dacd568665a760b56fb893050565f39
Jordy de Hoon <jordy.dehoon@hotmail.com>
2020-02-26 07:09:32 +0100

    Component VHDL done | NOT TESTED

--- Digitale-synthese.srcs/sources_1/new/EDGEDETECT.vhd
+++ Digitale-synthese.srcs/sources_1/new/EDGEDETECT.vhd
@@ -1,21 +1,35 @@
 -- JORDY DE HOON
 -- MODULE : EDGEDETECT
--- INFO : USING A STATE MACHINE TO TRACK THE 
+-- INFO : Using sig and a delay of sig to generate a puls if a rising edge is detected
 library ieee;
 use ieee.std_logic_1164.all;
 entity EDGEDETECT is
     port ( 
         sig     :   IN STD_LOGIC;   -- Input signal
+        clk     :   IN STD_LOGIC;   -- Clock signal
         reset   :   IN STD_LOGIC;   -- Active high reset
         edge    :   OUT STD_LOGIC   -- Edge detection signal
     );
 end EDGEDETECT;
 
+architecture Behavioral OF EDGEDETECT IS
+    SIGNAL edge_sig     : STD_LOGIC;
+    SIGNAL delay_sig    : STD_LOGIC;
+begin
 -- SYNC COMPONENT
 process(clk,reset) begin
-
-
+    if (reset = '1') then           -- Async Reset
+        delay_sig <= '0';
+    else
+        if(rising_edge(clk)) then   -- D flipflop | Delay function
+            delay_sig <= sig;
+        end if;
+    end if ;
 end process;
 -- COMBINATORIAL COMPONENT
-
+process(clk,delay_sig) begin
+    edge_sig <= sig AND (NOT delay_sig);    -- If SIG is HIGH and the delay low generate PULS
+end process;
 -- SIGNAL CONNECTIONS
+edge <= edge_sign;          -- Link to external connection
+end Behavioral;
