56cd5b0957ee429b9b9ce8203420327074bb2efd
Jordy de Hoon <jordy.dehoon@hotmail.com>
2020-02-27 09:57:24 +0100

    Module bug fixed: rising_edge on non-clk-signal

--- Digitale-synthese.srcs/sources_1/new/SEQUENCECONTROLLER.vhd
+++ Digitale-synthese.srcs/sources_1/new/SEQUENCECONTROLLER.vhd
@@ -9,6 +9,7 @@ use ieee.std_logic_unsigned.all;
 entity SEQUENCECONTROLLER is
     generic(COUNT_WIDTH : integer);
     port ( 
+            clk         : IN STD_LOGIC;     -- Clk signal
             reset       : IN STD_LOGIC;     -- Reset signal
             nextstate   : IN STD_LOGIC;     -- Input signal to next state
             sh_reg      : OUT STD_LOGIC;    -- Shift signal for registers (output)
@@ -19,37 +20,42 @@ end SEQUENCECONTROLLER;
 architecture Behavioral OF SEQUENCECONTROLLER IS
     SIGNAL BITCOUNT_curr     : STD_LOGIC_VECTOR(COUNT_WIDTH DOWNTO 0);
     SIGNAL BITCOUNT_next     : STD_LOGIC_VECTOR(COUNT_WIDTH DOWNTO 0);
+    SIGNAL BITCOUNT_fb       : STD_LOGIC;
     SIGNAL sh_next           : STD_LOGIC;
     SIGNAL ld_next           : STD_LOGIC;
 begin
+
 -- SYNC COMPONENT
 process(reset,nextstate) begin
     if (reset ='1') then
         BITCOUNT_curr(COUNT_WIDTH-1 DOWNTO 0) <= (OTHERS => '0'); -- sh flags
         BITCOUNT_curr(COUNT_WIDTH) = '1';   -- ld flag
     else
-        if (rising_edge(nextstate)) then
-            BITCOUNT_curr <= BITCOUNT_next;
-            sh_reg <= sh_next;
-            ld_reg <= ld_next;
-        elsif (falling_edge(nextstate))then
-            BITCOUNT_curr <= BITCOUNT_curr;
-            sh_reg <= '0';
-            ld_reg <= '0';
+        if (rising_edge(clk)) then
+            if(nextstate = '1')then
+                BITCOUNT_curr <= BITCOUNT_next;
+                sh_reg <= sh_next;
+                ld_reg <= ld_next;
+            else
+                BITCOUNT_curr <= BITCOUNT_curr;
+                sh_reg <= '0';
+                ld_reg <= '0';
+            end if;
         end if;
     end if;
 end process;
+
 -- COMBINATORIAL COMPONENT 
 process(BITCOUNT_curr) begin
-    BITCOUNT_next(COUNT_WIDTH DOWNTO 1) <= BITCOUNT_curr(COUNT_WIDTH-1 DOWNTO 0) -- Ring shifter
-    if (BITCOUNT_next(COUNT_WIDTH) = '1') then --ld flag
-        sh_next <= '0';
+    BITCOUNT_next<= BITCOUNT_curr rol 1;        -- Ring shifter
+    sh_next <= NOT ld_next;
+    if (BITCOUNT_cur(COUNT_WIDTH) = '1') then   -- ld flag
+       -- sh_next <= '0';
         ld_next <= '1';
     else
-        sh_next <= '1';
+       -- sh_next <= '1';
         ld_next <= '0';
     end if ;
 end process;
-
 -- SIGNAL CONNECTIONS
 end Behavioral;
