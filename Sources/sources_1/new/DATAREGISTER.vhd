740be56833edfb8ed2876bfa03d0080c875e399c
Jordy de Hoon <jordy.dehoon@hotmail.com>
2020-03-12 09:40:47 +0100

    God has helped

--- Sources/sources_1/new/DATAREGISTER.vhd
+++ Sources/sources_1/new/DATAREGISTER.vhd
@@ -8,8 +8,8 @@ entity DATAREGISTER is
 generic(SHIFT_WIDTH : integer);
 	port (
         clk     : IN STD_LOGIC;
-        ld      : IN STD_LOGIC;                                     -- load from the sequence controller
         reset   : IN STD_LOGIC;                                     -- Reset signal
+        ld      : IN STD_LOGIC;                                     -- load from the sequence controller
         sh      : IN STD_LOGIC;                                     -- shift data from the sequence controller
         data    : IN STD_LOGIC_VECTOR(SHIFT_WIDTH - 1 downto 0);    -- variavle input data
         sdo     : OUT STD_LOGIC                                     -- serial data out
@@ -17,25 +17,28 @@ generic(SHIFT_WIDTH : integer);
 end DATAREGISTER;
 
 architecture Behavioral OF DATAREGISTER IS
-	SIGNAL data_curr   : STD_LOGIC_VECTOR(SHIFT_WIDTH-1 DOWNTO 0);
+	SIGNAL data_next   : STD_LOGIC_VECTOR(SHIFT_WIDTH-1 DOWNTO 0);
 	SIGNAL data_pres   : STD_LOGIC_VECTOR(SHIFT_WIDTH-1 DOWNTO 0);
-	SIGNAL ld_old      : STD_LOGIC;
-	SIGNAL sh_old      : STD_LOGIC;
-	
-
 begin
 -- SYNC COMPONENT
 process(reset,clk) begin
     if (reset ='1') then
     data_pres <= (others => '0');
-    
     else
         if (rising_edge(clk)) then
             if ((sh = '1') AND (ld = '0')) then
-                --
+                data_pres <= data_next;
             elsif((sh = '0') AND (ld = '1')) then
-                --
+                data_pres <= data;
             end if;
         end if;
     end if;
 end process;
+-- COMB COMPONENT
+process(data_pres) begin
+    data_next(SHIFT_WIDTH-1 DONWTO 1) <= data_pres(SHIFT_WIDTH-2 DOWNTO 0);
+    data_next(0) <= '0';
+end process;
+--SIGNAL LINK
+sdo <= data_pres(SHIFT_WIDTH-1);
+end Behavioral;
