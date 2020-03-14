d2c5c1da8e43adbbba8b7aefe646b3fcda596318
Tomekske <joostenstomek@gmail.com>
2020-03-14 17:48:00 +0100

    Dataregister finished
    finished

--- Sources/sim_1/new/TB_DATAREGISTER.vhd
+++ Sources/sim_1/new/TB_DATAREGISTER.vhd
@@ -7,15 +7,18 @@ entity TB_DATAREGISTER is
 end TB_DATAREGISTER;
 
 architecture structural of TB_DATAREGISTER is 
-
+constant SHIFT_WIDTH : integer := 8;
 -- Component Declaration
 component DATAREGISTER
-port (
-    cha     :   IN STD_LOGIC;
-    clk     :   IN STD_LOGIC;
-    reset   :   IN STD_LOGIC;	-- Active high signal
-    syncha  :   OUT STD_LOGIC
-);
+generic(SHIFT_WIDTH : integer);
+	port (
+        clk     : IN STD_LOGIC;
+        reset   : IN STD_LOGIC;                                     -- Reset signal
+        ld      : IN STD_LOGIC;                                     -- load from the sequence controller
+        sh      : IN STD_LOGIC;                                     -- shift data from the sequence controller
+        data    : IN STD_LOGIC_VECTOR(SHIFT_WIDTH - 1 downto 0);    -- variable input data
+        sdo     : OUT STD_LOGIC                                     -- serial data out
+    );
 end component;
 
 for uut : DATAREGISTER use entity work.DATAREGISTER(Behavioral);
@@ -24,19 +27,18 @@ constant period     :   time := 100 ns;
 constant delay      :   time :=  10 ns;
 signal end_of_sim   :   boolean := false;
 
-signal clk      : std_logic;
-signal reset    : std_logic;
-signal cha      : std_logic;
-signal syncha   : std_logic;
+signal clk     : STD_LOGIC;
+signal reset   : STD_LOGIC;                                     -- Reset signal
+signal ld      : STD_LOGIC;                                     -- load from the sequence controller
+signal sh      : STD_LOGIC;                                     -- shift data from the sequence controller
+signal data    : STD_LOGIC_VECTOR(SHIFT_WIDTH - 1 downto 0);    -- variable input data
+signal sdo     : STD_LOGIC;     
 
 BEGIN
 
-uut: DATAREGISTER PORT MAP(
-    clk => clk,
-    cha => cha,
-    reset => reset,
-    syncha => syncha
-);
+uut: DATAREGISTER
+    generic map(SHIFT_WIDTH => SHIFT_WIDTH)
+    PORT MAP(clk, reset, ld, sh, data, sdo);
 
 clock : process
     begin 
@@ -55,37 +57,31 @@ clock : process
 end process clock;
     
 tb : PROCESS
-    procedure tbvector(constant stimvect : in std_logic_vector(1 downto 0)) is
+    procedure tbvector(constant stimvect : in std_logic_vector((10) downto 0)) is
     begin
-        reset <= stimvect(1);
-        cha <= stimvect(0);
+        reset <= stimvect(10);
+        ld <= stimvect(9);
+        sh <= stimvect(8);
+        data <= stimvect(7 downto 0);
         wait for period;
     end tbvector;
     BEGIN
     
-        -- doet niets
-        tbvector("00");
-        wait for period * 4;
+        -- Reset state (reset, ld, sh, data); expected -> data_pres = '0'
+        tbvector("10011001101");
+        wait for period * 2;
         
-        tbvector("01");
-        wait for period * 6;   
+        -- ld = 1; expected sdo = '1'
+        tbvector("01011001101");
+        wait for period * 2;
         
-        tbvector("10");
-        wait for period * 6;
+        -- sh = 1;expected sdo = '1'
+        tbvector("00111001101");
+        wait for period * 2;
         
-        -- reset
-        tbvector("10");
-        wait for period * 4;
-        
-        tbvector("11");
-        wait for period * 4;
-        
-        -- doe niets
-        tbvector("00");
-        wait for period * 4;
-        
-        tbvector("01");
-        wait for period * 10;
+        -- sh = 1;expected sdo = '0'
+        tbvector("00110110010");
+        wait for period * 2;
         
         end_of_sim <= true;
         wait;
--- Sources/sources_1/new/DATAREGISTER.vhd
+++ Sources/sources_1/new/DATAREGISTER.vhd
@@ -22,23 +22,35 @@ architecture Behavioral OF DATAREGISTER IS
 begin
 -- SYNC COMPONENT
 process(reset,clk) begin
-    if (reset ='1') then
-    data_pres <= (others => '0');
+    -- Async reset:
+    -- data_pres signal is initialised with logic zeros on an active reset
+    if (reset  = '1') then
+        data_pres <= (others => '0');
+    -- On every other values data_pres signal is initlialised with the calculated value
     else
+        -- Only initialise data_pres values on a rising clock edge
         if (rising_edge(clk)) then
+            -- On an active shift assign data_next signal to data_pres signal
             if ((sh = '1') AND (ld = '0')) then
                 data_pres <= data_next;
+            -- On an active load, assign the entity's input data to the data_pres signal
             elsif((sh = '0') AND (ld = '1')) then
                 data_pres <= data;
             end if;
         end if;
     end if;
 end process;
+
 -- COMB COMPONENT
 process(data_pres) begin
-    data_next(SHIFT_WIDTH-1 DONWTO 1) <= data_pres(SHIFT_WIDTH-2 DOWNTO 0);
+    -- data_next is calculted by bitshifting the array
+    -- data_next(SHIFT_WIDTH - 1 DOWNTO 1): only values between (MSB and LSB + 1) are assigned
+    -- data_pres(SHIFT_WIDTH - 2 DOWNTO 0): only value between(MSB - 1 and LSB) are shifted 
+    data_next(SHIFT_WIDTH - 1 DOWNTO 1) <= data_pres(SHIFT_WIDTH - 2 DOWNTO 0);
+    -- The LSB bit is assigned with a logic zero bit
     data_next(0) <= '0';
 end process;
 --SIGNAL LINK
+-- serial data out contains only one bit -> MSB
 sdo <= data_pres(SHIFT_WIDTH-1);
 end Behavioral;
