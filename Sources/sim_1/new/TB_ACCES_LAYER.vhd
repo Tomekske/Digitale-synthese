cb686975a19f4e08bfd235755a6309021be63279
JordyDH <jordy.dehoon@hotmail.com>
2020-03-17 13:51:37 +0100

    Moduel tested | test ok

--- Sources/sim_1/new/TB_ACCES_LAYER.vhd
+++ Sources/sim_1/new/TB_ACCES_LAYER.vhd
@@ -25,16 +25,17 @@ CONSTANT period : time := 100 ns;
 CONSTANT delay  : time :=  10 ns;
 SIGNAL end_of_sim : boolean := false;
 
-SIGNAL clk      : STD_LOGIC;
-SIGNAL reset    : STD_LOGIC;
-SIGNAL mode     : STD_LOGIC_VECTOR(1 DOWNTO 0);
-SIGNAL sdo      : STD_LOGIC;
+SIGNAL clk          : STD_LOGIC;
+SIGNAL reset        : STD_LOGIC;
+SIGNAL mode         : STD_LOGIC_VECTOR(1 DOWNTO 0);
+SIGNAL sdo          : STD_LOGIC;
+SIGNAL pn_start     : STD_LOGIC;
+SIGNAL sdo_spread   : STD_LOGIC;
 
 BEGIN
 
     uut: ACCES_LAYER
-    generic map(DATA_WIDTH => DATA_WIDTH)
-    PORT MAP(clk, reset, pn_start, data, sdo_out);
+    PORT MAP(clk, reset, sdo, mode, pn_start, sdo_spread);
 
     clock : process
     begin 
@@ -59,39 +60,39 @@ tb : PROCESS
     end tbvector;
     BEGIN
         -- Test async reset
-        stimvect("1000");   -- Reset active
+        tbvector("1000");   -- Reset active
         wait for period * 4;
-        stimvect("0000"); -- Reset disable : start process
+        tbvector("0000"); -- Reset disable : start process
         
-        wait until pn_start='1' -- Next sequence PN generator
-        stimvect("0000"); -- mux_out = sdo
+        wait until pn_start='1'; -- Next sequence PN generator
+        tbvector("0000"); -- mux_out = sdo
 
-        wait until pn_start='1'
-        stimvect("0100"); -- mux_out = sdo
+        wait until pn_start='1';
+        tbvector("0100"); -- mux_out = sdo
 
-        wait until pn_start='1'
-        stimvect("0000");
+        wait until pn_start='1';
+        tbvector("0000");
 
-        wait until pn_start='1'
-        stimvect("0101");
+        wait until pn_start='1';
+        tbvector("0101");
 
-        wait until pn_start='1'
-        stimvect("0001");
+        wait until pn_start='1';
+        tbvector("0001");
 
-        wait until pn_start='1'
-        stimvect("0110");
+        wait until pn_start='1';
+        tbvector("0110");
 
-        wait until pn_start='1'
-        stimvect("0010");
+        wait until pn_start='1';
+        tbvector("0010");
 
-        wait until pn_start='1'
-        stimvect("0111");
+        wait until pn_start='1';
+        tbvector("0111");
 
-        wait until pn_start='1'
-        stimvect("0011");
+        wait until pn_start='1';
+        tbvector("0011");
 
-        wait until pn_start='1'
-        stimvect("0000");
+        wait until pn_start='1';
+        tbvector("0000");
 
         --end of sim
         end_of_sim <= true;
