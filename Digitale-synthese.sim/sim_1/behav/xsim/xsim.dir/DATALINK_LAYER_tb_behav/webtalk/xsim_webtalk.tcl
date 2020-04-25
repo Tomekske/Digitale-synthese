7f89883e693c526b011420ebe911a7e9e86ed921
JordyDH <jordy.dehoon@hotmail.com>
2020-04-25 13:31:09 +0200

    ignore file added AGAIN stupid git

--- Digitale-synthese.sim/sim_1/behav/xsim/xsim.dir/DATALINK_LAYER_tb_behav/webtalk/xsim_webtalk.tcl
+++ Digitale-synthese.sim/sim_1/behav/xsim/xsim.dir/DATALINK_LAYER_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/Jordy/KU/Digitale-synthese/Digitale-synthese.sim/sim_1/behav/xsim/xsim.dir/DATALINK_LAYER_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Mon Mar 16 14:00:18 2020" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Tue Mar 17 15:22:27 2020" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2019.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2708876" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -12,13 +12,13 @@ webtalk_add_data -client project -key target_family -value "not_applicable" -con
 webtalk_add_data -client project -key target_device -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key target_package -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
-webtalk_add_data -client project -key random_id -value "4c690c4f-b997-4515-bb86-085a9e632543" -context "software_version_and_target_device"
+webtalk_add_data -client project -key random_id -value "4cab7b99-57f1-4b9f-9d3a-a792cc72f7b0" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "901cc444230848afb22f640eb4811a29" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "5" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "8" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
-webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-6500U CPU @ 2.50GHz" -context "user_environment"
-webtalk_add_data -client project -key cpu_speed -value "2592 MHz" -context "user_environment"
+webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-3770 CPU @ 3.40GHz" -context "user_environment"
+webtalk_add_data -client project -key cpu_speed -value "3392 MHz" -context "user_environment"
 webtalk_add_data -client project -key total_processors -value "1" -context "user_environment"
 webtalk_add_data -client project -key system_ram -value "17.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
@@ -27,7 +27,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "6750 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "5888_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3600539530 -regid "" -xml C:/Users/Jordy/KU/Digitale-synthese/Digitale-synthese.sim/sim_1/behav/xsim/xsim.dir/DATALINK_LAYER_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Jordy/KU/Digitale-synthese/Digitale-synthese.sim/sim_1/behav/xsim/xsim.dir/DATALINK_LAYER_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Jordy/KU/Digitale-synthese/Digitale-synthese.sim/sim_1/behav/xsim/xsim.dir/DATALINK_LAYER_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.00_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "5904_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 3544241967 -regid "" -xml C:/Users/Jordy/KU/Digitale-synthese/Digitale-synthese.sim/sim_1/behav/xsim/xsim.dir/DATALINK_LAYER_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Jordy/KU/Digitale-synthese/Digitale-synthese.sim/sim_1/behav/xsim/xsim.dir/DATALINK_LAYER_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Jordy/KU/Digitale-synthese/Digitale-synthese.sim/sim_1/behav/xsim/xsim.dir/DATALINK_LAYER_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
--- Digitale-synthese.sim/sim_1/behav/xsim/xsim.dir/SEQUENCECONTROLLER_tb_behav/webtalk/xsim_webtalk.tcl
+++ Digitale-synthese.sim/sim_1/behav/xsim/xsim.dir/SEQUENCECONTROLLER_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
-webtalk_init -webtalk_dir C:/Users/Jordy/KU/Digitale-synthese/Digitale-synthese.sim/sim_1/behav/xsim/xsim.dir/TRANSMITTER_tb_behav/webtalk/
+webtalk_init -webtalk_dir C:/Users/Jordy/KU/Digitale-synthese/Digitale-synthese.sim/sim_1/behav/xsim/xsim.dir/SEQUENCECONTROLLER_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Tue Mar 17 14:55:08 2020" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Tue Mar 17 15:11:03 2020" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2019.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2708876" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "4cab7b99-57f1-4b9f-9d3a-a792cc72f7b0" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "901cc444230848afb22f640eb4811a29" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "5" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "3" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-3770 CPU @ 3.40GHz" -context "user_environment"
@@ -25,9 +25,9 @@ webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key runall -value "true" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "396850 ns" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "12150 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.06_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6380_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1314889627 -regid "" -xml C:/Users/Jordy/KU/Digitale-synthese/Digitale-synthese.sim/sim_1/behav/xsim/xsim.dir/TRANSMITTER_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Jordy/KU/Digitale-synthese/Digitale-synthese.sim/sim_1/behav/xsim/xsim.dir/TRANSMITTER_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Jordy/KU/Digitale-synthese/Digitale-synthese.sim/sim_1/behav/xsim/xsim.dir/TRANSMITTER_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.00_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "5896_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 2640872341 -regid "" -xml C:/Users/Jordy/KU/Digitale-synthese/Digitale-synthese.sim/sim_1/behav/xsim/xsim.dir/SEQUENCECONTROLLER_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Jordy/KU/Digitale-synthese/Digitale-synthese.sim/sim_1/behav/xsim/xsim.dir/SEQUENCECONTROLLER_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Jordy/KU/Digitale-synthese/Digitale-synthese.sim/sim_1/behav/xsim/xsim.dir/SEQUENCECONTROLLER_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
