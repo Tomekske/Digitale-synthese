d89d57f04f788fa9cf899983b5e9140f80d01555
JordyDH <jordy.dehoon@hotmail.com>
2020-05-07 16:55:51 +0200

    Module added

--- vhdl_template.vhd
+++ vhdl_template.vhd
@@ -9,19 +9,39 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use ieee.std_logic_unsigned.all;
 
-entity "MODULE NAME HERE" is
-    port( 
+entity DATASHIFTREG is
+    generic(SHIFT_WIDTH : integer);
+    port(
+        reset       : IN STD_LOGIC;
+        clk         : IN STD_LOGIC;
+        sdi         : IN STD_LOGIC;
+        sh          : IN STD_LOGIC;
+        pdo         : OUT STD_LOGIC_VECTOR(SHIFT_WIDTH - 1 DOWNTO 0)
             
     );
-end "MODULE NAME HERE";
+end DATASHIFTREG;
 
-architecture Behavioral OF "MODULE NAME HERE" IS
--- SIGNALS HERE
+architecture Behavioral OF DATASHIFTREG IS
+    SIGNAL data_pres        : STD_LOGIC_VECTOR(SHIFT_WIDTH - 1 DOWNTO 0);
+    SIGNAL data_next        : STD_LOGIC_VECTOR(SHIFT_WIDTH - 1 DOWNTO 0);
 begin
 -- SYNC COMPONENT HERE
-
+process(clk,reset)begin
+    if(reset = '1')then
+        data_pres <= (OTHERS => '0');
+    else
+        if(rising_edge(clk))then
+            if(sh = '1')then
+                data_pres <= data_next;
+            end if;
+        end if;
+    end if;
+end process;
 -- COMB COMPONENT HERE
-
+process(data_pres)begin
+    data_next(SHIFT_WIDTH - 1 DOWNTO 1) <= data_pres (SHIFT_WIDTH - 2 DOWNTO 0);
+    data_next(0) <= sdi;
+end 
 -- LINKING SIGNALS HERE
-
+pdo <= data_pres; 
 end Behavioral;
